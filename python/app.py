b3b29a417517c2872aeaba286c7cbed3475b5a66
Douglas Wu <wckdouglas@gmail.com>
2023-01-03 23:14:13 -0500

    streaming

--- python/app.py
+++ python/app.py
@@ -1,16 +1,54 @@
-from flask import Flask, request
+from time import sleep
+
+import cv2
+from flask import (
+    Flask,
+    Response,
+    jsonify,
+    render_template,
+    request,
+    stream_with_context,
+    url_for,
+)
 from skimage import io
 from webcam_filter import apply_filter
 
 app = Flask(__name__, template_folder="./templates")
 
 
-@app.route("/")
+def stream_template(template_name, **context):
+    app.update_template_context(context)
+    t = app.jinja_env.get_template(template_name)
+    rv = t.stream(context)
+    rv.enable_buffering(5)
+    return rv
+
+
+def stream_video():
+    # define a video capture object
+    vid = cv2.VideoCapture(0)
+    while True:
+        ret, frame = vid.read()
+        yield frame
+
+
+def stream_emoji():
+    for frame in stream_video():
+        yield "\n".join(apply_filter(frame, debug=False))
+
+
+@app.route("/_stream")
 def stream():
     # TODO: accept stream video
-    img = "/Users/wckdouglas/Desktop/test.png"
-    frame = io.imread(img)
-    return "<br>".join(apply_filter(frame, debug=False))
+    # frame = io.imread("/Users/wckdouglas/Desktop/test.png")
+    # return render_template("index.html", processed_string="<br>".join(apply_filter(frame, debug=False)))
+    img = next(stream_emoji())
+    return jsonify(img=img)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
--- python/config.py
+++ python/config.py
@@ -10,4 +10,4 @@ LEARNING_RATE = 0.01
 MINIBATCH_SIZE = 30
 BASE_DIR = Path(__name__).absolute().parents[1]
 TRAINING_STATE_OUTPUT_DIR = BASE_DIR / "python" / "train_state"
-EMOJI_IMG_SHAPE = (50, 72)
+EMOJI_IMG_SHAPE = (50, 70)
